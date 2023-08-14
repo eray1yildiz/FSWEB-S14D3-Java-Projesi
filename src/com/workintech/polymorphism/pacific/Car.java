e9638e81b7017ace815be79d19824e4fb366676e
Eray Yıldız <eray1yildiz@hotmail.com>
2023-08-15 01:31:07 +0300

    tamamlandı

---
+++ src/com/workintech/polymorphism/pacific/Car.java
@@ -0,0 +1,72 @@
+package com.workintech.polymorphism.pacific;
+
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        setEngine(true);
+        setWheels(4);
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setEngine(boolean engine) {
+        this.engine = engine;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+    public String startEngine(){
+        System.out.println("Class Name: " + getClass().getSimpleName());
+        return "the car's engine is starting";
+    }
+    public String accelerate(){
+        System.out.println("Class Name: " + getClass().getSimpleName());
+        return "the car is accelerating";
+    }
+    public String brake(){
+        System.out.println("Class Name: " + getClass().getSimpleName());
+        return "the car is braking";
+    }
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        return ((Car) o).cylinders == this.cylinders && ((Car) o).name.equals(this.name);
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        build.append("\n" + "Name: " + name + "\n");
+        build.append("Cylinders: " + cylinders + "\n");
+        return build.toString();
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
---
+++ src/com/workintech/polymorphism/pacific/Main.java
@@ -0,0 +1,23 @@
+package com.workintech.polymorphism.pacific;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Car ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Car holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
+
+    }
+}
